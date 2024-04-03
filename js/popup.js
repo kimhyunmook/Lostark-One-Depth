90a7b1fc99487f33512b852eaa90aa3b129c1bcd
Kimhyunmook <gusanr4200@naver.com>
2024-04-04 00:48:53 +0900

    .

--- js/popup.js
+++ js/popup.js
@@ -6,7 +6,7 @@ export default function Popup() {
   });
   let popup = ce({
     className: `
-    popup absolute w-[80%] pt-10 pb-12 pr-9 pl-9 bg-[rgba(255,255,255,0.9)] z-30 left-1/2 top-1/2 translate-x-[-50%] 
+    popup absolute w-[80%] md:w-[30%] pt-10 pb-12 pr-9 pl-9 md:pt-7 md:pb-9 md:pr-5 md:pl-5 bg-[rgba(255,255,255,0.9)] z-30 left-1/2 top-1/2 translate-x-[-50%] 
     translate-y-[-50%] rounded-lg flex items-center justify-center flex-wrap
     `,
     inner: title("공지사항", ""),
@@ -15,16 +15,20 @@ export default function Popup() {
   ce({
     className: "closebtn",
     append: popup,
-  });
+  }).addEventListener('click', (e) => {
+    e.preventDefault();
+    const t = e.currentTarget.parentNode.parentNode;
+    t.remove();
+  })
   ce({
     element: "p",
     className: "text-lg w-full break-keep",
     inner: `안녕하세요 <b class="text-xl mr-1 ml-1">LostArk One Depth Spec</b>입니다. <br> 
       원하는 캐릭터의 스팩을 계산해 점수화 시켜 보여줍니다.  <br>
     점수 계산 방식은 제작자의 실제 경험을 통해 만들었으며 
-    유저마다 기준은 다를 수 있습니다. <br>
+    유저마다 기준은 다를 수 있기에 참고만 부탁드립니다. <br>
     <br>
-    p.s 구인 구직을 좀 더 편하게 하기 위해 만들었으며 스팩 자체를 빡빡하게 검사하지 않습니다.
+    <b>p.s</b> 구인 구직을 좀 더 편하게 하기 위해 만들었으며 스팩 자체를 빡빡하게 검사하지 않습니다.
       `,
     append: popup,
   });
